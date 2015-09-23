9e0a3a7abcf7bb81e737bb2bdb6e378a207bc782
Henrik Langer <henni19790@googlemail.com>
2015-09-23 19:31:36 +0200

    Audio driver modifications for ad193x. Driver not tested yet.

--- sound/soc/bcm/audiocard.c
+++ sound/soc/bcm/audiocard.c
@@ -3,8 +3,10 @@
  *
  * Author:	Henrik Langer <henni19790@googlemail.com>
  *		Copyright 2015
- *              based on RaspiDAC3 driver by Jan Grulich <jan@grulich.eu>
- *		based on BlackFin-AD193x driver by Barry Song <Barry.Song@analog.com>
+ *        	based on 
+ * 				RaspiDAC3 driver by Jan Grulich <jan@grulich.eu>,
+ *				BlackFin-AD193x driver by Barry Song <Barry.Song@analog.com>
+ *				SuperAudioBoard driver by R F William Hollender <whollender@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,62 +20,108 @@
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
- 
+
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 #include <sound/jack.h>
-
-//TODO: May include dma, irq headers, etc. (see bf5xx-ad193x.c)
+#include <sound/soc-dapm.h>
 
 #include "../codecs/ad193x.h"
 
 /* sound card init */
 static int snd_rpi_audiocard_init(struct snd_soc_pcm_runtime *rtd)
 {
+	int ret;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct snd_soc_codec *codec = rtd->codec;
+	struct snd_soc_card *card = rtd->card;
+
+	// set codec DAI slots, 8 channels, all channels are enabled
+	// codec driver ignores TX and RX mask
+	ret = snd_soc_dai_set_tdm_slot(codec_dai, 0xFF, 0xFF, 2, 32);
+	if (ret < 0){
+		dev_err(codec->dev, "Unable to set AD193x TDM slots.");
+		return ret;
+	}
+
+	// set codec DAI format 
+	// (ad193x driver only supports SND_SOC_DAIFMT_DSP_A and SND_SOC_DAIFMT_I2S with TDM)
+	ret = snd_soc_dai_set_fmt(codec_dai, SND_SOC_DAIFMT_I2S);
+	if (ret < 0){
+		dev_err(codec->dev, "Unable to set codec DAI format.");
+		return ret;
+	}
+
+	// set the cpu DAI (tdm with more than 2 channels not available in platform driver)
+	// cpu DAI only supports SND_SOC_DAIFMT_I2S (see bcm2835-i2s.c)
+	// => for multichannel support, platform driver may have to be modified
+	ret = snd_soc_dai_set_fmt(cpu_dai, SND_SOC_DAIFMT_I2S);
+	if (ret < 0){
+		dev_err(card->dev, "Unable to set cpu DAI format.");
+		return ret;
+	}
 
 	return 0;
 }
 
 /* set hw parameters */
-static int snd_rpi_raspidac3_hw_params(struct snd_pcm_substream *substream,
+static int snd_rpi_audiocard_hw_params(struct snd_pcm_substream *substream,
 				       struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct snd_soc_codec *codec = rtd->codec;
 
-	unsigned int sample_bits =
-		snd_pcm_format_physical_width(params_format(params));
+	// Need to tell the codec what it's system clock is (12.288 MHz crystal)
+	int sysclk = 12288000;
 
-	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2);
+	ret = snd_soc_dai_set_sysclk(codec_dai, 0, sysclk, SND_SOC_CLOCK_IN); // clk_id is ignored in ad193x driver
+	if (ret < 0){
+		dev_err(codec->dev, "Unable to set AD193x system clock.");
+		return ret;
+	}
+
+	unsigned int sample_bits = snd_pcm_format_physical_width(params_format(params));
+
+	// bclk_ratio should be 256 (mclk / 48 KHz)
+	// blck ratio of CS4272 is always 64 in master => mode snd_soc_dai_set_bclk_ratio(cpu_dai, 64)
+	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2); //why * 2? (took from raspidac3.c)
 }
 
 /* startup */
-static int snd_rpi_raspidac3_startup(struct snd_pcm_substream *substream) {
+static int snd_rpi_audiocard_startup(struct snd_pcm_substream *substream) {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_codec *codec = rtd->codec;
-	snd_soc_update_bits(codec, PCM512x_GPIO_CONTROL_1, 0x08,0x08);
-	tpa6130a2_stereo_enable(codec, 1);
+	//snd_soc_update_bits(codec, PCM512x_GPIO_CONTROL_1, 0x08,0x08);
+	//tpa6130a2_stereo_enable(codec, 1);
 	return 0;
 }
 
 /* shutdown */
-static void snd_rpi_raspidac3_shutdown(struct snd_pcm_substream *substream) {
+static void snd_rpi_audiocard_shutdown(struct snd_pcm_substream *substream) {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_codec *codec = rtd->codec;
-	snd_soc_update_bits(codec, PCM512x_GPIO_CONTROL_1, 0x08,0x00);
-	tpa6130a2_stereo_enable(codec, 0);
+	//snd_soc_update_bits(codec, PCM512x_GPIO_CONTROL_1, 0x08,0x00);
+	//tpa6130a2_stereo_enable(codec, 0);
 }
 
 /* machine stream operations */
-static struct snd_soc_ops snd_rpi_raspidac3_ops = {
-	.hw_params = snd_rpi_raspidac3_hw_params,
-	.startup = snd_rpi_raspidac3_startup,
-	.shutdown = snd_rpi_raspidac3_shutdown,
+static struct snd_soc_ops snd_rpi_audiocard_ops = {
+	.hw_params = snd_rpi_audiocard_hw_params,
+	.startup = snd_rpi_audiocard_startup,
+	.shutdown = snd_rpi_audiocard_shutdown,
 };
 
 /* interface setup */
+#define AUDIOCARD_AD193X_DAIFMT (
+				SND_SOC_DAIFMT_I2S | \
+				SND_SOC_DAIFMT_IB_IF | \ //not sure, but should be supported by platform (took from Blackfin AD193x driver)
+				SND_SOC_DAIFMT_CBM_CFM) 
+
 static struct snd_soc_dai_link snd_rpi_audiocard_dai[] = {
 	{
 		.name = "AudioCard",
@@ -81,10 +129,9 @@ static struct snd_soc_dai_link snd_rpi_audiocard_dai[] = {
 		.cpu_dai_name = "bcm2708-i2s.0",
 		.codec_dai_name ="ad193x-hifi",
 		.platform_name = "bcm2708-i2s.0",
-		.codec_name = "spi0.5",
-		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_IF | \
-				SND_SOC_DAIFMT_CBM_CFM,
-		.ops = &snd_rpi_raspidac3_ops,
+		.codec_name = "spi0.5", //TODO: spi<bus #>.<CS #> => not sure for RPi
+		.dai_fmt = AUDIOCARD_AD193X_DAIFMT,
+		.ops = &snd_rpi_audiocard_ops,
 		.init = snd_rpi_audiocard_init,
 	},
 };
@@ -92,37 +139,65 @@ static struct snd_soc_dai_link snd_rpi_audiocard_dai[] = {
 /* audio machine driver */
 static struct snd_soc_card bf5xx_ad193x = {
 	.name = "snd_rpi_audiocard",
-	.owner = THIS_MODULE,
 	.dai_link = snd_rpi_audiocard_dai,
 	.num_links = ARRAY_SIZE(snd_rpi_audiocard_dai),
 };
 
-/* sound card disconnect */
-static int snd_rpi_raspidac3_remove(struct platform_device *pdev)
+/* sound card test */
+static int snd_rpi_audiocard_probe(struct platform_device *pdev)
 {
-	return snd_soc_unregister_card(&snd_rpi_raspidac3);
+	int ret = 0;
+
+	snd_rpi_audiocard.dev = &pdev->dev;
+
+	if (pdev->dev.of_node) {
+	    struct device_node *i2s_node;
+	    struct snd_soc_dai_link *dai = &snd_rpi_audiocard_dai[0];
+	    i2s_node = of_parse_phandle(pdev->dev.of_node,
+					"i2s-controller", 0);
+
+	    if (i2s_node) {
+		dai->cpu_dai_name = NULL;
+		dai->cpu_of_node = i2s_node;
+		dai->platform_name = NULL;
+		dai->platform_of_node = i2s_node;
+	    }
+	}
+
+	ret = snd_soc_register_card(&snd_rpi_audiocard);
+	if (ret)
+		dev_err(&pdev->dev,
+			"snd_soc_register_card() failed: %d\n", ret);
+
+	return ret;
 }
 
-static const struct of_device_id raspidac3_of_match[] = {
-	{ .compatible = "jg,raspidacv3", },
+/* sound card disconnect */
+static int snd_rpi_audiocard_remove(struct platform_device *pdev)
+{
+	return snd_soc_unregister_card(&snd_rpi_audiocard);
+}
+
+static const struct of_device_id snd_rpi_audiocard_of_match[] = {
+	{ .compatible = "audiocard,audiocard", },
 	{},
 };
-MODULE_DEVICE_TABLE(of, raspidac3_of_match);
+MODULE_DEVICE_TABLE(of, snd_rpi_audiocard_of_match);
 
 /* sound card platform driver */
-static struct platform_driver snd_rpi_raspidac3_driver = {
+static struct platform_driver snd_rpi_audiocard_driver = {
 	.driver = {
-		.name   = "snd-rpi-raspidac3",
+		.name   = "snd-rpi-audiocard",
 		.owner  = THIS_MODULE,
-		.of_match_table = raspidac3_of_match,
+		.of_match_table = snd_rpi_audiocard_of_match,
 	},
-	.probe          = snd_rpi_raspidac3_probe,
-	.remove         = snd_rpi_raspidac3_remove,
+	.probe          = snd_rpi_audiocard_probe,
+	.remove         = snd_rpi_audiocard_remove,
 };
 
-module_platform_driver(snd_rpi_raspidac3_driver);
+module_platform_driver(snd_rpi_audiocard_driver);
 
 /* Module information */
-MODULE_AUTHOR("Barry Song");
+MODULE_AUTHOR("Henrik Langer");
 MODULE_DESCRIPTION("ALSA SoC AD193X board driver");
 MODULE_LICENSE("GPL");
